5914954fbbb824b3f5eb85a001dda3aa0a08f11d
Remka <remuka@gmail.com>
2018-07-18 20:57:57 +0900

    Removing config & adding config.example

---
+++ config/secret.example.js
@@ -0,0 +1,12 @@
+var config = {
+  mongoUrl: 'mongodb://<username>:<password>@<url>:<port>/<folder>',
+  mailHost: '<hosturl>',
+  mailPort: 587,
+  mailSecure: false,
+  mailAuth: {
+    user: '<username>',
+    pass: '<password>'
+  }
+}
+
+module.exports = config;
